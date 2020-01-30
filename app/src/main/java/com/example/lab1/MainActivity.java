09b3dda8cd9ca85fe26d48d9ba4eb044b6563cf8
dunkelmoses <moses.hashim@gmail.com>
2020-01-30 12:58:39 -0500

    lab3

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,16 +1,43 @@
 package com.example.lab1;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences prefs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_relative);
-       
+        setContentView(R.layout.email);
+
+        prefs = getSharedPreferences("FileName", Context.MODE_PRIVATE);
+        String savedString = prefs.getString("ReserveName", "");
+
+        EditText typeField = findViewById(R.id.edEmail);
+        typeField.setText(savedString);
+        Button saveButton = findViewById(R.id.button);
+
+        saveButton.setOnClickListener( v -> {
+            Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+            String emailTyped = typeField.getText().toString();
+            goToProfile.putExtra("EMAIL", emailTyped);
+            saveSharedPrefs( emailTyped );
+            startActivity(goToProfile);
+        });
+    }
+
+    private void saveSharedPrefs(String stringToSave)
+    {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("ReserveName", stringToSave);
+        editor.apply();
     }
 }
---
+++ app/src/main/java/com/example/lab1/ProfileActivity.java
@@ -0,0 +1,80 @@
+package com.example.lab1;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageButton mImageButton;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+            Log.e(ACTIVITY_NAME,"In onActivityResult");
+
+            }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        mImageButton = findViewById(R.id.imageButton);
+        mImageButton.setOnClickListener(v ->
+        {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+        Log.e(ACTIVITY_NAME,"In onCreate()");
+
+        Intent dataSent = getIntent();
+        String emailSent = dataSent.getStringExtra("EMAIL");
+        String formattedString = String.format(emailSent);
+        EditText email = findViewById(R.id.EditText2);
+        email.setText(formattedString);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME, "In onStop()" );
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME, "In onStart()");
+
+        }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME, "In onResume()");
+
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME, "In onDestroy()");
+
+    }
+}
